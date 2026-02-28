1faea2d3639f7a51e79d4b1692a76b31eec233c3
Stylianos Tzouvaras <stylianos.tzouvaras@netcompany.com>
2026-02-28 10:18:39 +0200

    feat: Disable forced PID activation
    The `forcePidActivation` property in `ConfigLogic` has been updated to return `false` by default, disabling the requirement for mandatory PID activation in the wallet.

--- business-logic/src/main/java/eu/europa/ec/businesslogic/config/ConfigLogic.kt
+++ business-logic/src/main/java/eu/europa/ec/businesslogic/config/ConfigLogic.kt
@@ -57,7 +57,7 @@ interface ConfigLogic {
     /**
      * Set if the wallet requires PID Activation.
      */
-    val forcePidActivation: Boolean get() = true
+    val forcePidActivation: Boolean get() = false
 }
 
 enum class AppFlavor {
